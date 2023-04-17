bae23031c95d80a7032d9239f43795138abc894f
juancamilorc <crcamiloramirez@hotmail.com>
2023-04-17 08:43:32 -0500

    actualizacion sidebar

--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -16,7 +16,16 @@ export class SidebarService {
         { titulo: 'ProgressBar', url: 'progress'},
         { titulo: 'Rxjs', url: 'rxjs'},
       ]
-    }
+    },
+    {
+      titulo: 'Mantenimiento',
+      icono: 'mdi mdi-folder-lock-open',
+      submenu : [
+        { titulo: 'Usuarios', url: '/usuarios'},
+        { titulo: 'Hospitales', url: 'hospitales'},
+        { titulo: 'Médicos', url: 'medicos'},
+      ]
+    },
   ]
 
   constructor() { }
